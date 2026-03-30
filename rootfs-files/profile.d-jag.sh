443e452b146f499c765636bd71e7a2379bfd36aa
Gendosik <gendosik@jaglinux.com>
2026-03-30 08:35:44 +0300

    fix: remove all heredocs from workflow

---
+++ rootfs-files/profile.d-jag.sh
@@ -0,0 +1,7 @@
+export PS1='[JAG \w]# '
+alias ll='ls -lh --color=auto'
+alias la='ls -A --color=auto'
+echo ""
+echo "  Welcome to JAG Linux v1.0 beta!"
+echo "  Type jag-help for available commands."
+echo ""
